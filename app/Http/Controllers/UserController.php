dda113feb8f789ced4bd7aba514f15d510ef3b05
aryod24 <aryowahyu24@gmail.com>
2024-09-20 17:01:19 +0700

    JS4 P1-$fillable

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,34 +10,15 @@ class UserController extends Controller
 {
     public function index()
     {
-        /* coba akses model UserModel
-         $user = UserModel::all(); // ambil semua data dari tabel m_user
-         return view('user', ['data' => $user]);
-        
-
-        // tambah data user dengan eloquent model
          $data = [
-             'username' => 'customer-1',
-             'nama' => 'Pelanggan',
-             'password' => Hash::make('12345'),
-             'level_id' => 3
+             'level_id' => 2,
+             'username' => 'manager_tiga',
+             'nama' => 'Manager 3',
+             'password' => Hash::make('12345')
          ];
-         UserModel::insert($data); //tambahkan data ke tabel m_ser
+         UserModel::create($data); 
 
-        //coba akses model userModel
-         $user = UserModel::all(); //ambil semua data dari tabel m_user
+         $user = UserModel::all(); 
          return view('user', ['data' => $user]);
-        */
-        // tambah data user dengan Eloquent Model
-        $data = [
-            'nama' => 'Pelanggan Pertama',
-        ];
-        UserModel::where('username', 'customer-1')->update($data); // update user data
-
-        // retrieve updated user data
-        $users = UserModel::all();
-
-        return view('user', ['data' => $users]); // return the view with updated user data
-
     }
 }
