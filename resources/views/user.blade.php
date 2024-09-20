e5086c2ef44e09596f9449c09cb56d760bef22ee
aryod24 <aryowahyu24@gmail.com>
2024-09-20 19:32:18 +0700

    JS4 P2.6-Create, Read, Update, Delete (CRUD)

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,26 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Data User</title>
+    <title>Data user</title>
 </head>
 <body>
-    <h1>Data User</h1>
+    <h1>Data user</h1>
+    <a href="{{url('/user/tambah')}}">+ Tambah User</a>
     <table border="1" cellpadding="2" cellspacing="0">
         <tr>
-            <th>ID</th>
-            <th>Username</th>
-            <th>Nama</th>
-            <th>ID Level Pengguna</th>
+            <td>ID</td>
+            <td>Username</td>
+            <td>Nama</td>
+            <td>ID Level pengguna</td>
+            <td>Aksi</td>
         </tr>
+        @foreach ($data as $d)
         <tr>
-            <td>{{ $data->user_id }}</td>
-            <td>{{ $data->username }}</td>
-            <td>{{ $data->nama }}</td>
-            <td>{{ $data->level_id }}</td>
+            <td> {{$d->user_id}} </td>
+            <td> {{$d->username}} </td>
+            <td> {{$d->nama}} </td>
+            <td> {{$d->level_id}} </td>
+            <td><a href="{{ url('/user/ubah/' . $d->user_id) }}">Ubah</a> | <a href="{{ url('/user/hapus/' . $d->user_id) }}">Hapus</a>
         </tr>
+        @endforeach
     </table>
-</body>
-</html>
+</body></html>
--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-
 });
+
+// level
 Route::get('/level', [LevelController::class, 'index']);
+// kategori
 Route::get('/kategori', [KategoriController::class, 'index']);
+//user
 Route::get('/user', [UserController::class, 'index']);
+
+Route::get('/user/tambah', [UserController::class, 'tambah']);
+Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
+
+Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
+Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
+Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
